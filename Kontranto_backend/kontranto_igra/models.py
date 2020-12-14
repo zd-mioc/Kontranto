3ca9c23f0a49ed4ec83638ba33920f5b45d7a95e
NBrala7890 <nikbrala@gmail.com>
2020-12-14 13:55:14 +0100

    Popravljen model baze (models.py)

--- Kontranto_backend/kontranto_igra/models.py
+++ Kontranto_backend/kontranto_igra/models.py
@@ -1,21 +1,17 @@
 from django.db import models
-
+ 
 class Game(models.Model):
     game_id=models.CharField(max_length=200)
     white_player_id=models.CharField(max_length=200)
     black_player_id=models.CharField(max_length=200)
     white_score=models.IntegerField()
     black_score=models.IntegerField()
-    white_triangle_position=models.CharField(max_length=200)
-    black_triangle_postition=models.CharField(max_length=200)
-    white_circle_position=models.CharField(max_length=200)
-    black_circle_position=models.CharField(max_length=200)
+    board=models.JSONField()
+    game_state=models.CharField(max_length=200)
 
 class Move(models.Model):
-    game_id=models.ForeignKey("game", on_delete=models.CASCADE)
-    white_triangle_position=models.CharField(max_length=200)
-    black_triangle_postion=models.CharField(max_length=200)
-    white_circle_position=models.CharField(max_length=200)
-    black_circle_position=models.CharField(max_length=200)
-    last_move_white_timestamp=models.DateTimeField()
-    last_move_black_timestamp=models.DateTimeField()
+    game_id=models.ForeignKey("Game", on_delete=models.CASCADE)
+    color=models.CharField(max_length=200)
+    triangle_position=models.CharField(max_length=10)
+    circle_position=models.CharField(max_length=10)
+    move_timestamp=models.DateTimeField()
