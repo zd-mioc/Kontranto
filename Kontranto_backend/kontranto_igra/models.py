7ddb0ec98e8ae4e4b493def56cf0a51c96a15256
LovroD77359 <68549602+LovroD77359@users.noreply.github.com>
2021-02-26 11:38:54 +0100

    Dodano polje locked

--- Kontranto_backend/kontranto_igra/models.py
+++ Kontranto_backend/kontranto_igra/models.py
@@ -9,6 +9,7 @@ class Game(models.Model):
     black_score=models.IntegerField()
     board=models.JSONField()
     game_state=models.CharField(max_length=200)
+    locked=models.JSONField()
 
 class Move(models.Model):
     game=models.ForeignKey(Game, on_delete=models.CASCADE)
