669bc34382b8e11278a541a097a0a11d0cb32884
NBrala7890 <68543139+NBrala7890@users.noreply.github.com>
2021-02-27 19:17:54 +0100

    Update views.py

--- Kontranto_backend/kontranto_igra/views.py
+++ Kontranto_backend/kontranto_igra/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpRequest, request, response
 from django.http import HttpResponse
-from kontranto_igra.game_logic import new_game_f, join_game_f, make_move, get_game_state
+from django.template import loader, RequestContext
+from django.middleware.csrf import get_token
+from kontranto_igra.game_logic import new_game_f, check_game_new, join_game_f, check_game_join, game_state_f, get_move_f, make_move
 import json
+
 
 def index(request):
     return render(request, "kontranto_igra/homepage.html")
@@ -10,21 +14,48 @@ def index(request):
 def pravila(request):
     return render(request, "kontranto_igra/pravila.html")
 
-def show_board(request):
-    return render(request, "kontranto_igra/board.html")
+def show_board(request): #tek ako je "status": "OK" kod new/join poziv ide ovdje
+    game_id = request.POST.get("game_id")
+    player_id = request.POST.get("player_id")
+    csrf_token = get_token(request)
+    csrf_token_html = '<input type="hidden" name="csrfmiddlewaretoken" value="{}" />'.format(csrf_token)
+    if game_id == "to_be_set": #ide na new_game opciju
+        template = loader.get_template("kontranto_igra/board.html")
+        newgamedata = new_game_f(player_id)
+        response_body = template.render({"status" : newgamedata["status"], "game_id": newgamedata["game_id"], "my_id": player_id, "my_color" : newgamedata["my_color"], "csrf" : csrf_token})
+        return HttpResponse(response_body)
+    else: #ide na join_game opciju
+        template = loader.get_template("kontranto_igra/board.html")
+        joingamedata = join_game_f(game_id, player_id)
+        response_body = template.render({"status" : joingamedata["status"], "game_id": game_id, "my_id": joingamedata["my_id"], "my_color" : joingamedata["my_color"], "csrf": csrf_token})
+        # context_instance=RequestContext(request))
+        return HttpResponse(response_body)
+
 
 def new_game(request):
     body_unicode = request.body.decode('utf-8')
     jsonFromBody = json.loads(body_unicode)
-    player_1_id = jsonFromBody['player_1_id']
-    return HttpResponse(new_game_f(player_1_id), content_type="application/json")
+    player_id = jsonFromBody['player_id']
+    # player_id = request.POST.get("player_id") - ne radi
+    new_game_resp = check_game_new(player_id) #prvo radi provjeru
+    if new_game_resp["status"] == "OK": #ako je provjera prosla, salje ispravan json
+        return HttpResponse(json.dumps(new_game_resp), content_type="application/json")
+    else: #inace salje krivi, pa se poziv ne moze poslati
+        return HttpResponse(new_game_resp, content_type="application/json")
 
 def join_game(request):
     body_unicode = request.body.decode('utf-8')
     jsonFromBody = json.loads(body_unicode)
     game_id = jsonFromBody['game_id']
-    player_2_id = jsonFromBody['player_2_id']
-    return HttpResponse(join_game_f(game_id, player_2_id), content_type="application/json")
+    player_id = jsonFromBody['player_id']
+    get_game_resp = check_game_join(game_id, player_id) #prvo radi provjeru
+    if get_game_resp["status"] == "OK": #ako je provjera prosla, salje ispravan json
+        return HttpResponse(json.dumps(get_game_resp), content_type="application/json")
+    else: #inace salje krivi, pa se poziv ne moze poslati
+        return HttpResponse(get_game_resp, content_type="application/json")
+
+def prijelaz(request):
+    return render(request, "kontranto_igra/prijelaz.html")
 
 def move(request):
     body_unicode = request.body.decode('utf-8')
@@ -35,8 +66,21 @@ def move(request):
     new_circle_position = jsonFromBody['new_circle_position']
     return HttpResponse(make_move(game_id, player_id, new_triangle_position, new_circle_position), content_type="application/json")
 
-def board_state(request):
+def game_state(request): #uzimamo state, id od drugog igraca
     body_unicode = request.body.decode('utf-8')
     jsonFromBody = json.loads(body_unicode)
     game_id = jsonFromBody['game_id']
-    return HttpResponse(get_game_state(game_id), content_type="application/json")
+    my_color = jsonFromBody['my_color']
+    return HttpResponse(game_state_f(game_id, my_color), content_type="application/json")
+
+def get_move(request):
+    body_unicode = request.body.decode('utf-8')
+    jsonFromBody = json.loads(body_unicode)
+    game_id = jsonFromBody['game_id']
+    opponent_color = jsonFromBody['opponent_color']
+    ntp = jsonFromBody['ntp']
+    ncp = jsonFromBody['ncp']
+    return HttpResponse(get_move_f(game_id, opponent_color, ntp, ncp), content_type="application/json")
+
+def update_info(request):
+    return render(request, "kontranto_igra/board.html")
