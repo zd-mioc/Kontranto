0bb44f2730ac7ca4a783a2b7f35cc6c2f4e540b5
NBrala7890 <68543139+NBrala7890@users.noreply.github.com>
2021-02-27 19:18:26 +0100

    Update urls.py

--- Kontranto_backend/kontranto_igra/urls.py
+++ Kontranto_backend/kontranto_igra/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path("new_game", views.new_game, name="new_game"),
     path("join_game", views.join_game, name="join_game"),
     path("move", views.move, name="move"),
-    path("board_state", views.board_state, name="board_state")
+    path("game_state", views.game_state, name="game_state"),
+    path("prijelaz", views.prijelaz, name="prijelaz"),
+    path("get_move", views.get_move, name="get_move")
+    # path("update_info", views.update_info, name="update_info")
 ]
