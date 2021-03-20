928f8e6ca1c8c136878d0dd036321053ab461049
Ivan Krišto <ivan.kristo@gmail.com>
2021-03-20 14:51:44 +0100

    Promjeni ID polje tako da je autoincrement

--- kontranto_igra/models.py
+++ kontranto_igra/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Game(models.Model):
-    id=models.IntegerField(primary_key=True) #ovo je pk od Game-a, jer ForeignKey prima integer, pa ce se Move ovim povezati
     game=models.CharField(max_length=200)
     white_player_id=models.CharField(max_length=200)
     black_player_id=models.CharField(max_length=200)
