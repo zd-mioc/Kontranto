1716dfbe3d737f8b062b61923466239ec477816a
Ivan Krišto <ivan.kristo@gmail.com>
2021-03-20 13:54:16 +0100

    Dodaj STATIC_ROOT u settings

--- Kontranto/settings.py
+++ Kontranto/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,4 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
