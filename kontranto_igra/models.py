fd5f0cdf9d0ee369148342f6948547082e693fd5
Ivan <ivan.kristo@gmail.com>
2022-01-22 15:07:33 +0100

    handle moves on the client

--- kontranto_igra/models.py
+++ kontranto_igra/models.py
@@ -8,16 +8,20 @@ class GameState(enum.Enum):
   """All possible game states."""
   # Game is created, but still missing the second player.
   GAME_SETUP = enum.auto()
+  # The game is done. No further action is possible.
+  FINISHED = enum.auto()
   # Both players are present. First play step: color choice
   COLOR_CHOICE = enum.auto()
   # The players are moving their figures.
   GAME_RUNNING = enum.auto()
+  # The black player submitted the next move. Waiting for the white.
+  WAITING_WHITE_PLAYER_MOVE = enum.auto()
+  # The white player submitted the next move. Waiting for the black.
+  WAITING_BLACK_PLAYER_MOVE = enum.auto()
   # A clash between figures occured.
   CLASH = enum.auto()
   # Shock move triggered.
   SHOCK_MOVE = enum.auto()
-  # The game is done. No further action is possible.
-  FINISHED = enum.auto()
 
 
 def default_board():
@@ -33,6 +37,21 @@ class Game(models.Model):
 
   white_player_score=models.IntegerField(default=0)
   black_player_score=models.IntegerField(default=0)
+  # 4x4 grid with the following fields:
+  #  '' -- empty field
+  #  WT -- white triangle
+  #  WC -- white circle
+  #  BT -- black triangle
+  #  BC -- black circle
+  #  SF -- shock field
+  #  DF -- destroyed field
+  # When player sent new move, but it's not yet official
+  # (waiting for the other player's move):
+  #  NWT -- next white triangle
+  #  NWC -- next white circle
+  #  NBT -- next black triangle
+  #  NBC -- next black circle
+  # These are not for the clients.
   board=models.JSONField(default=default_board)
 
   possible_states = [(s.name, s.name) for s in GameState]
